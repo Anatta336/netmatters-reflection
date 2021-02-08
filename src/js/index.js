56510c7f0b7f930a8e0aec290aae523807432f30
Anatta336 <sdriver@gmx.co.uk>
2021-02-08 16:32:06 +0000

    add carousel

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,17 @@
 import Glide from '@glidejs/glide';
 
+// unhide anything waiting to know that JavaScript is available
+document.querySelectorAll('.hiddenIfNoScript').forEach((element) => {
+  element.style.display = 'block';
+});
+
 const glide = new Glide('.glide', {
   type: 'carousel',
   startAt: 0,
   perView: 1,
   perTouch: 1,
+  gap: 0,
   autoplay: 4500,
   hoverpause: true,
+  animationDuration: 250,
 }).mount();
