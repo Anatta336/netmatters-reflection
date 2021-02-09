22fea8733df21a93fd9098bca2e19c6740ad9ada
Anatta336 <sdriver@gmx.co.uk>
2021-02-09 11:59:44 +0000

    add babel to build process

--- .webpack.config.js
+++ .webpack.config.js
@@ -1,10 +1,26 @@
 const path = require('path');
-
 module.exports = {
-  entry: './src/js/index.js',
+  entry: {
+    app: ['core-js/stable', './src/js/index.js']
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist/js'),
   },
-  mode: 'development',
-};
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  devtool: "cheap-source-map",
+  mode: 'development'
+}
