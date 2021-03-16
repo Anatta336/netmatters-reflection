e307394dd36f2ed2e68b6797988d3fab003a6528
Sam Driver <sdriver@gmx.co.uk>
2021-03-16 15:13:15 +0000

    add runQuery

--- src/php/database/DatabaseInterface.php
+++ src/php/database/DatabaseInterface.php
@@ -5,6 +5,7 @@ interface DatabaseInterface
 {
     /**
      * Executes a given SQL query, optionally binding values to replace placeholders.
+     * Returns results as an associative array.
      * @param string $sqlQuery The SQL query to execute, with any placeholders
      *                         marked by '?'.
      * @param mixed ...$values 0 or more values that will be bound to placeholders
@@ -12,4 +13,15 @@ interface DatabaseInterface
      * @return array Array of associative arrays which holds the results.
      */
     public function fetchResults(string $sqlQuery, ...$values): array;
+    
+    /**
+     * Executes a given SQL query, optionally binding values to replace placeholders.
+     * Returns boolean indicating if query was executed.
+     * @param string $sqlQuery The SQL query to execute, with any placeholders
+     *                         marked by '?'.
+     * @param mixed ...$values 0 or more values that will be bound to placeholders
+     *                         in the query.
+     * @return bool True if query was executed successfully.
+     */
+    public function runQuery(string $sqlQuery, ...$values): bool;
 }
--- src/php/database/SQLiteDatabase.php
+++ src/php/database/SQLiteDatabase.php
@@ -18,6 +18,7 @@ class SQLiteDatabase implements DatabaseInterface
                 ]
             );
         } catch (\Exception $e) {
+            // TODO: log this instead of a random echo
             echo "Unable to access SQLite database at $pathToFile\n"
                 . $e->getMessage();
         }
@@ -25,13 +26,36 @@ class SQLiteDatabase implements DatabaseInterface
 
     public function fetchResults(string $sqlQuery, ...$values): array
     {
-        $statement = $this->pdo->prepare($sqlQuery);
-
-        for ($i = 0; $i < count($values); $i++) {
-            $statement->bindValue($i + 1, $values[$i]);
+        try {
+            $statement = $this->pdo->prepare($sqlQuery);
+            
+            for ($i = 0; $i < count($values); $i++) {
+                $statement->bindValue($i + 1, $values[$i]);
+            }
+            $statement->execute();
+        } catch (\Exception $e) {
+            // TODO: log this
+            echo "Unable to fetch results.\n"
+                . $e->getMessage();
         }
-        $statement->execute();
-
         return $statement->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    public function runQuery(string $sqlQuery, ...$values): bool
+    {
+        $result = false;
+        try {
+            $statement = $this->pdo->prepare($sqlQuery);
+
+            for ($i = 0; $i < count($values); $i++) {
+                $statement->bindValue($i + 1, $values[$i]);
+            }
+            $result = $statement->execute();
+        } catch (\Exception $e) {
+            // TODO: log this
+            echo "Unable to store values.\n"
+                . $e->getMessage();
+        }
+        return $result;
     }
 }
